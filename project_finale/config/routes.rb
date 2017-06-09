e1eb86fbfc7442c809f03e9aae794f9bf92340c5
Olufunsho Olagundoye <Funsholag@gmail.com>
2017-06-09 17:48:02 -0400

    error with devise sign out fixed

--- project_finale/config/routes.rb
+++ project_finale/config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   get '/' => "home#index"
   root :to => "home#index"
   get '/aboutus' => "about#index"
